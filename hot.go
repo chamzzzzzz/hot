f452f67afe2f6e41258a9f5850e4426d52e43ab7
Liu Chen <chamzzzzzz@gmail.com>
2022-10-11 08:40:40 +0800

    add hot url,catagory fields

--- hot.go
+++ hot.go
@@ -5,9 +5,11 @@ import (
 )
 
 type Hot struct {
-	Title   string
-	Summary string
-	Date    time.Time
+	Title    string
+	Summary  string
+	URL      string
+	Catagory string
+	Date     time.Time
 }
 
 type Board struct {
@@ -21,14 +23,51 @@ func NewBoard(name string) *Board {
 	}
 }
 
+func (b *Board) Append0(hot *Hot) *Hot {
+	b.Hots = append(b.Hots, hot)
+	return hot
+}
+
 func (b *Board) Append(title, summary string, date time.Time) *Hot {
+	return b.Append3(title, summary, date)
+}
+
+func (b *Board) Append5(title, summary, url, catagory string, date time.Time) *Hot {
+	hot := &Hot{
+		Title:    title,
+		Summary:  summary,
+		URL:      url,
+		Catagory: catagory,
+		Date:     date,
+	}
+	return b.Append0(hot)
+}
+
+func (b *Board) Append4(title, summary, url string, date time.Time) *Hot {
+	hot := &Hot{
+		Title:   title,
+		Summary: summary,
+		URL:     url,
+		Date:    date,
+	}
+	return b.Append0(hot)
+}
+
+func (b *Board) Append3(title, summary string, date time.Time) *Hot {
 	hot := &Hot{
 		Title:   title,
 		Summary: summary,
 		Date:    date,
 	}
-	b.Hots = append(b.Hots, hot)
-	return hot
+	return b.Append0(hot)
+}
+
+func (b *Board) Append2(title string, date time.Time) *Hot {
+	hot := &Hot{
+		Title: title,
+		Date:  date,
+	}
+	return b.Append0(hot)
 }
 
 type Crawler interface {
