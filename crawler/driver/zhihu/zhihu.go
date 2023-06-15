9123f19f07bc5327414f661c112539143825a63e
Liu Chen <chamzzzzzz@gmail.com>
2023-06-15 22:45:57 +0800

    add zhihu again

---
+++ crawler/driver/zhihu/zhihu.go
@@ -0,0 +1,54 @@
+package zhihu
+
+import (
+	"strings"
+
+	"github.com/chamzzzzzz/hot"
+	"github.com/chamzzzzzz/hot/crawler/driver"
+	"github.com/chamzzzzzz/hot/crawler/httputil"
+)
+
+const (
+	DriverName  = "zhihu"
+	ProxySwitch = false
+	URL         = "https://www.zhihu.com/topsearch"
+)
+
+type Driver struct {
+}
+
+func (driver *Driver) Open(option driver.Option) (driver.Crawler, error) {
+	return &Crawler{Option: option}, nil
+}
+
+func init() {
+	driver.Register(DriverName, &Driver{})
+}
+
+type Crawler struct {
+	Option driver.Option
+}
+
+func (c *Crawler) Driver() driver.Driver {
+	return &Driver{}
+}
+
+func (c *Crawler) Name() string {
+	return DriverName
+}
+
+func (c *Crawler) Crawl() (*hot.Board, error) {
+	dom := &httputil.DOM{}
+	if err := httputil.Request("GET", URL, nil, "dom", dom, httputil.NewOption(c.Option, ProxySwitch)); err != nil {
+		return nil, err
+	}
+
+	board := hot.NewBoard(c.Name())
+	for _, div := range dom.QueryAll("div", "class", "TopSearchMain-title") {
+		title := strings.TrimSpace(div.Text())
+		board.Append(&hot.Hot{
+			Title: title,
+		})
+	}
+	return board, nil
+}
--- crawler/drivers.go
+++ crawler/drivers.go
@@ -133,5 +133,6 @@ import (
 	_ "github.com/chamzzzzzz/hot/crawler/driver/zaker"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/zaobao"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/zdnet"
+	_ "github.com/chamzzzzzz/hot/crawler/driver/zhihu"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/zol"
 )
