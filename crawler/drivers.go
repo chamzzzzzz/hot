925fa085d59e5c09ad6666529fbd39a5b9c0b1d3
Liu Chen <chamzzzzzz@gmail.com>
2024-04-13 22:46:50 +0800

    add nbd

--- crawler/drivers.go
+++ crawler/drivers.go
@@ -70,6 +70,7 @@ import (
 	_ "github.com/chamzzzzzz/hot/crawler/driver/maoyan"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/mydrivers"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/mysmth"
+	_ "github.com/chamzzzzzz/hot/crawler/driver/nbd"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/netease"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/nytimes"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/odaily"
