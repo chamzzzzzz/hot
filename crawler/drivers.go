e16d31ffef8d84b9c5d38d5639ddf5f0bb9a0205
Liu Chen <chamzzzzzz@gmail.com>
2024-04-16 18:46:19 +0800

    add jingji21

--- crawler/drivers.go
+++ crawler/drivers.go
@@ -61,6 +61,7 @@ import (
 	_ "github.com/chamzzzzzz/hot/crawler/driver/ithome"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/jiemian"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/jin10"
+	_ "github.com/chamzzzzzz/hot/crawler/driver/jingji21"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/jisilu"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/jqka10"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/kdslife"
