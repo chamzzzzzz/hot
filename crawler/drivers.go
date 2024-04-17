811a702f78fada7a5fc935002c840f422c6e0806
Liu Chen <chamzzzzzz@gmail.com>
2024-04-17 19:59:59 +0800

    add blockbeats

--- crawler/drivers.go
+++ crawler/drivers.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/chamzzzzzz/hot/crawler/driver/bilibili"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/bjnews"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/bkeconomy"
+	_ "github.com/chamzzzzzz/hot/crawler/driver/blockbeats"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/btc8"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/chinanews"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/chinaz"
