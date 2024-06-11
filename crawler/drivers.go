1b6c13bc9d058ed34e194a6e38b5527880fa666c
Liu Chen <chamzzzzzz@gmail.com>
2024-06-11 22:29:55 +0800

    remove hellogithub

--- crawler/drivers.go
+++ crawler/drivers.go
@@ -54,7 +54,6 @@ import (
 	_ "github.com/chamzzzzzz/hot/crawler/driver/guozaoke"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/haokan"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/haoyou3839"
-	_ "github.com/chamzzzzzz/hot/crawler/driver/hellogithub"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/hexun"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/hibor"
 	_ "github.com/chamzzzzzz/hot/crawler/driver/hket"
